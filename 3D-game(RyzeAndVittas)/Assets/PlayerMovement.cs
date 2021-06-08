4bf196ed22fda325af37f5d3cee94d73bf8495eb
Ryze420 <84809085+ryze420@users.noreply.github.com>
2021-06-08 19:16:15 +0300

    asd

--- 3D-game(RyzeAndVittas)/Assets/PlayerMovement.cs
+++ 3D-game(RyzeAndVittas)/Assets/PlayerMovement.cs
@@ -5,21 +5,42 @@ public class PlayerMovement : MonoBehaviour
 {
     //here i take reference from the rigidbody and call it rb
     public Rigidbody rb;
+    public Transform player;
+    public PlayerMovement movement;
 
     public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
-    float horizontalSpeed = 2.0f;
-    float verticalSpeed = 2.0f;
 
     // Update is called once per frame
 
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey("d"))
         {
-            rb.AddForce(0, 0, 10 * Time.deltaTime);
+            rb.AddForce(speed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime, ForceMode.VelocityChange);
         }
     }
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "wall")
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            movement.enabled = false;
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+        }
 
+    }
 }
 
