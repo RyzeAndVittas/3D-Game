8fef3de6350ab81fa17e1decb1757ee34964f3ed
Ryze420 <84809085+ryze420@users.noreply.github.com>
2021-06-08 18:00:19 +0300

    i just try somethings

--- 3D-game(RyzeAndVittas)/Assets/PlayerMovement.cs
+++ 3D-game(RyzeAndVittas)/Assets/PlayerMovement.cs
@@ -1,34 +1,25 @@
-
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
     //here i take reference from the rigidbody and call it rb
     public Rigidbody rb;
 
-    public float runspeed = 10f;
-    
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = 2.0f;
 
     // Update is called once per frame
+
     void Update()
     {
-        if (Input.GetKey("w"))
-        {
-            rb.AddForce(0, 0, runspeed * Time.deltaTime, ForceMode.VelocityChange);
-        }
         if (Input.GetKey("d"))
         {
-            rb.AddForce(runspeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            rb.AddForce(0, 0, 10 * Time.deltaTime);
         }
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-runspeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(0, 0, -runspeed * Time.deltaTime, ForceMode.VelocityChange);
-        }
-        
-
     }
+
 }
+
