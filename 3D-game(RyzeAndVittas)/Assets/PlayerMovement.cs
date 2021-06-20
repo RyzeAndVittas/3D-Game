47569b8191d1a95eae0382605e9d8b52c1b18861
Ryze420 <84809085+ryze420@users.noreply.github.com>
2021-06-21 02:20:34 +0300

    asd

--- 3D-game(RyzeAndVittas)/Assets/PlayerMovement.cs
+++ 3D-game(RyzeAndVittas)/Assets/PlayerMovement.cs
@@ -6,37 +6,24 @@ public class PlayerMovement : MonoBehaviour
     //here i take reference from the rigidbody and call it rb
     public Rigidbody rb;
     public Transform player;
-    
-
-
-
-    public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
-
-    void Update()
-    {
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
-        float translation = Input.GetAxis("Horizontal") * speed;
-        float rotation = Input.GetAxis("Vertical") * rotationSpeed;
-
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        // Move translation along the object's z-axis
-        transform.Translate(translation, 0, 0);
-        transform.Translate(0, 0, rotation);
-
-
-
-
-
-
-    }
-
+    bool corner = false;
+    public float cd = 1f;
    
     
+
+    void FixedUpdate()
+    {
+        if (corner = false && Input.GetKey("d"))
+        {
+            rb.AddForce(100, 0, 0);
+            Debug.Log("d is pressed");
+        }
+        if (player.position == new Vector3(6, 1, 6))
+        {
+            corner = true;
+        }
+    }
+
+ 
 
 }
